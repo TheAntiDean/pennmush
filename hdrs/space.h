0a6da6501e8f9530ca5e82113524f7cb289aab3e
Dean Santos <dean@santos.ws>
2022-08-08 04:04:20 +1000

    Code sync

--- hdrs/space.h
+++ hdrs/space.h
@@ -310,16 +310,16 @@ struct spaceconfig {
 	double cochrane_rate;
 };
 
-// #if __GNUC__ > 10 || \
-//     (__GNUC__ == 10 && (__GNUC_MINOR__ > 0 || \
-//                        (__GNUC_MINOR__ == 0 && \
-//                         __GNUC_PATCHLEVEL__ > 0)))
-// extern intmap *border_map;
-// extern HASHTAB aspace_consoles;
-// #else
+#if __GNUC__ > 10 || \
+    (__GNUC__ == 10 && (__GNUC_MINOR__ > 0 || \
+                       (__GNUC_MINOR__ == 0 && \
+                        __GNUC_PATCHLEVEL__ > 0)))
+extern intmap *border_map;
+extern HASHTAB aspace_consoles;
+#else
 intmap *border_map;
 HASHTAB aspace_consoles;
-//#endif
+#endif
 
 typedef struct spaceconfig SPACETAB;
 extern SPACETAB configstruct;
